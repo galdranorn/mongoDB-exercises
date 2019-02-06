724ac00493df2ec61a2a7bcb64c03e0b53df5df6
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-02-06 15:45:10 +0100

    Create base model

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useMongoClient: true
+})
+
+// new user schema
+const userSchema = new Schema({
+    name: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true},
+    admin: Boolean,
+    created_at: Date,
+    updated_at: Date
+})
+
+// mongoose schema method
+userSchema.methods.manify = function(next){
+    this.name = this.name + '-boy';
+    return next(null, this.name);
+}
+
+// pre-save method
+userSchema.pre('save', function(next){
+    const currentDate = new Date(); // take current time
+    this.updated_at = currentDate;
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
 })
